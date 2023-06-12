2faa87b617b8b0ee4c9ed9c61742b83313f5eb3f
calebbenjin <calebben94@gmail.com>
2023-06-12 10:18:06 +0100

    project setup and customFont

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,31 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import localFont from '@next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const zonapro = localFont({
+  src: [
+    {
+      path: '../public/fonts/ZonaPro-Light.otf',
+      weight: '300'
+    },
+    {
+      path: '../public/fonts/ZonaPro.otf',
+      weight: '400'
+    },
+    {
+      path: '../public/fonts/ZonaPro-SemiBold.otf',
+      weight: '600'
+    },
+    {
+      path: '../public/fonts/ZonaPro-Bold.otf',
+      weight: '700'
+    },
+    {
+      path: '../public/fonts/ZonaPro-ExtraBold.otf',
+      weight: '800'
+    }
+  ],
+  variable: '--font-zona'
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +38,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${zonapro.variable} font-sans`}>
+      <body>{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-zona)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
