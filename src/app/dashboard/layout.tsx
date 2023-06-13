d19144a5d619c378206ceae3eaa692852a8cb60e
calebbenjin <calebben94@gmail.com>
2023-06-13 14:19:28 +0100

    chore: dashoard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,9 @@
+import { Sidenav } from '@/components';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      <header>Hello</header>
+      <Sidenav />
       <main>{children}</main>
       <footer>Footer</footer>
     </section>
---
+++ src/components/Sidenav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidenav = () => {
+  return (
+    <div>
+      <h1>SideNav</h1>
+    </div>
+  );
+};
+
+export default Sidenav;
